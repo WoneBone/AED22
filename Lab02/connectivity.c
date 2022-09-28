e6e5bfe44800e7f578d05e38b937550b963e6086
WoneBone <nuno.g.tribolet.de.abreu@tecnico.ulisboa.com>
2022-09-28 11:18:54 +0100

    Lab 02 : Exercícios pre-lab e modificação do connectivity.c

---
+++ Lab02/connectivity.c
@@ -0,0 +1,313 @@
+/******************************************************************************
+ * (c) 2010-2019 AED Team
+ * Last modified: abl 2019-02-22
+ *
+ * NAME
+ *   connectivity.c
+ *
+ * DESCRIPTION
+ *   Algorithms for solving the connectivity problem -  QF QU WQU CWQU
+ *   For each method count number of entry pairs and the number of links
+ *
+ * COMMENTS
+ *   Code for public distribution
+ ******************************************************************************/
+#include<stdio.h>
+#include<stdlib.h>
+
+#include "connectivity.h"
+
+#define DEBUG 0
+
+/******************************************************************************
+ * quick_find()
+ *
+ * Arguments: id - array with connectivity information
+ *            N - size of array
+ *            fp - file pointer to read data from
+ *            quietOut - to reduce output to just final count
+ * Returns: (void)
+ * Side-Effects: pairs of elements are read and the connectivity array is
+ *               modified
+ *
+ * Description: Quick Find algorithm
+ *****************************************************************************/
+
+void quick_find(int *id, int N, FILE * fp, int quietOut)
+{
+   int i, p, q, t;
+   int pairs_cnt = 0;            /* connection pairs counter */
+   int links_cnt = 0;            /* number of links counter */
+   unsigned long long int elementar_s=0, elementar_u = 0;
+   /* initialize; all disconnected */
+   for (i = 0; i < N; i++) {
+      id[i] = i; //+1
+   }
+
+   /* read while there is data */
+   while (fscanf(fp, "%d %d", &p, &q) == 2) {
+      pairs_cnt++;
+      /* do search first */
+      if (id[p] == id[q]) { //+2
+      elementar_s=elementar_s+2;
+         /* already in the same set; discard */
+#if (DEBUG == 1)
+         printf("\t%d %d\n", p, q);
+#endif
+         continue;
+      }
+    elementar_s=elementar_s+2;   
+
+      /* pair has new info; must perform union */
+      for (t = id[p], i = 0; i < N; i++) {//+1
+         if (id[i] == t) {//+1
+            id[i] = id[q];//+2
+             elementar_u+=2;
+         }
+         elementar_u++;
+      }
+      elementar_u++;
+      links_cnt++;
+      if (!quietOut)
+         printf(" %d %d\n", p, q);
+   }
+   printf("QF: The number of links performed is %d for %d input pairs.\n",
+          links_cnt, pairs_cnt);
+   printf("Number of elemental operations in search: %llu\nNumber of elemental operations in union: %llu\n",elementar_s, elementar_u);
+   /*for (i = 0 ; i < N; i++){
+      q = 0;
+      for (t = 0; t < N; t++){
+         if (id[t] == i) q = printf("%d - ", t);
+      } 
+      if (q != 0) printf("\n");
+   }*/
+   return;
+}
+
+
+/******************************************************************************
+ * quick_union()
+ *
+ * Arguments: id - array with connectivity information
+ *            N - size of array
+ *            fp - file pointer to read data from
+ *            quietOut - to reduce output to just final count
+ * Returns: (void)
+ * Side-Effects: pairs of elements are read and the connectivity array is
+ *               modified
+ *
+ * Description: Quick Union algorithm
+ *****************************************************************************/
+
+void quick_union(int *id, int N, FILE * fp, int quietOut)
+{
+
+   int i, j, p, q;
+   int pairs_cnt = 0;            /* connection pairs counter */
+   int links_cnt = 0;            /* number of links counter */
+   unsigned long long int elementar_s=0, elementar_u = 0;
+   /* initialize; all disconnected */
+   for (i = 0; i < N; i++) {
+      id[i] = i;
+   }
+
+   /* read while there is data */
+   while (fscanf(fp, "%d %d", &p, &q) == 2) {
+      pairs_cnt++;
+      i = p;
+      j = q;
+
+      /* do search first */
+      while (i != id[i]) {//+1
+         elementar_s=elementar_s+2;
+         i = id[i];//+1
+      }
+      elementar_s++;
+      while (j != id[j]) {//+1
+         elementar_s=elementar_s+2;
+         j = id[j];//+1
+      }
+      elementar_s++;
+      if (i == j) {
+         /* already in the same set; discard */
+#if (DEBUG == 1)
+         printf("\t%d %d\n", p, q);
+#endif
+         continue;
+      }
+
+      /* pair has new info; must perform union */
+      id[i] = j;//+1
+      elementar_u++;
+      links_cnt++;
+
+      if (!quietOut)
+         printf(" %d %d\n", p, q);
+   }
+   printf("QU: The number of links performed is %d for %d input pairs.\n",
+          links_cnt, pairs_cnt);
+  printf("Number of elemental operations in search: %llu\nNumber of elemental operations in union: %llu\n",elementar_s, elementar_u);  
+}
+
+
+/******************************************************************************
+ * weighted_quick_union()
+ *
+ * Arguments: id - array with connectivity information
+ *            N - size of array
+ *            fp - file pointer to read data from
+ *            quietOut - to reduce output to just final count
+ * Returns: (void)
+ * Side-Effects: pairs of elements are read and the connectivity array is 
+ *               modified
+ *
+ * Description: Weighted Quick Union algorithm
+ *****************************************************************************/
+
+void weighted_quick_union(int *id, int N, FILE * fp, int quietOut)
+{
+
+   int i, j, p, q;
+   int *sz = (int *) malloc(N * sizeof(int));
+   int pairs_cnt = 0;            /* connection pairs counter */
+   int links_cnt = 0;            /* number of links counter */
+   static int elementar_s=0, elementar_u = 0;
+
+   /* initialize; all disconnected */
+   for (i = 0; i < N; i++) {
+      id[i] = i;
+      sz[i] = 1;
+   }
+
+   /* read while there is data */
+   while (fscanf(fp, "%d %d", &p, &q) == 2) {
+      pairs_cnt++;
+
+      /* do search first */
+      for (i = p; i != id[i]; i = id[i]){
+         elementar_s=elementar_s+2;
+      }
+      for (j = q; j != id[j]; j = id[j]){
+         elementar_s=elementar_s+2;
+      }
+      
+      if (i == j) {
+         /* already in the same set; discard */
+#if (DEBUG == 1)
+         printf("\t%d %d\n", p, q);
+#endif
+         continue;
+      }
+
+      /* pair has new info; must perform union; pick right direction */
+      if (sz[i] < sz[j]) {
+         id[i] = j;
+         sz[j] += sz[i];
+         elementar_u=elementar_u+5;
+      }
+      else {
+         id[j] = i;
+         sz[i] += sz[j];
+         elementar_u=elementar_u+5;
+      }
+      links_cnt++;
+
+      if (!quietOut)
+         printf(" %d %d\n", p, q);
+   }
+   printf("WQU: The number of links performed is %d for %d input pairs.\n",
+          links_cnt, pairs_cnt);
+
+   printf("Number of elemental operations in search: %d\nNumber of elemental operations in union: %d\n",elementar_s, elementar_u);
+
+   free(sz);
+   return;
+}
+
+
+/******************************************************************************
+ * compressed_weighted_quick_union()
+ *
+ * Arguments: id - array with connectivity information
+ *            N - size of array
+ *            fp - file pointer to read data from
+ *            quietOut - to reduce output to just final count
+ * Returns: (void)
+ * Side-Effects: pairs of elements are read and the connectivity array is
+ *               modified
+ *
+ * Description: Compressed Weighted Quick Union algorithm
+ *****************************************************************************/
+
+void compressed_weighted_quick_union(int *id, int N, FILE * fp, int quietOut)
+{
+   int i, j, p, q, t, x;
+   int *sz = (int *) malloc(N * sizeof(int));
+   int pairs_cnt = 0;            /* connection pairs counter */
+   int links_cnt = 0;            /* number of links counter */
+   static int elementar_s=0, elementar_u = 0;
+
+
+   /* initialize; all disconnected */
+   for (i = 0; i < N; i++) {
+      id[i] = i;
+      sz[i] = 1;
+   }
+
+   /* read while there is data */
+   while (fscanf(fp, "%d %d", &p, &q) == 2) {
+      pairs_cnt++;
+
+      /* do search first */
+      for (i = p; i != id[i]; i = id[i]){
+         elementar_s=elementar_s+2;
+      }
+      for (j = q; j != id[j]; j = id[j]){
+         elementar_s=elementar_s+2;
+      }
+
+      if (i == j) {
+         /* already in the same set; discard */
+#if (DEBUG == 1)
+         printf("\t%d %d\n", p, q);
+#endif
+         continue;
+      }
+
+      /* pair has new info; must perform union; pick right direction */
+      if (sz[i] < sz[j]) {
+         id[i] = j;
+         sz[j] += sz[i];
+         t = j;
+         elementar_u=elementar_u+5;
+      }
+      else {
+         id[j] = i;
+         sz[i] += sz[j];
+         t = i;
+         elementar_u=elementar_u+5;
+      }
+      links_cnt++;
+
+      /* retrace the path and compress to the top */
+      for (i = p; i != id[i]; i = x) {
+         x = id[i];
+         id[i] = t;
+         elementar_u=elementar_u+3;
+      }
+      for (j = q; j != id[j]; j = x) {
+         x = id[j];
+         id[j] = t;
+         elementar_u=elementar_u+3;
+      }
+      if (!quietOut)
+         printf(" %d %d\n", p, q);
+   }
+   printf("CWQU: The number of links performed is %d for %d input pairs.\n",
+          links_cnt, pairs_cnt);
+   
+   printf("Number of elemental operations in search: %d\nNumber of elemental operations in union: %d\n",elementar_s, elementar_u);
+
+   free(sz);
+   return;
+}
