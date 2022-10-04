2e9c22280d25d76e82e6a16778742070e3ab48c2
Alex-Ares <ares.santos@tecnico.ulisboa.pt>
2022-10-04 15:07:58 +0100

    updated lab03

--- Lab03/pt1/palTab.c
+++ Lab03/pt1/palTab.c
@@ -102,18 +102,18 @@ void AlocaTabelaPalavras ( char *ficheiro, st_texto *t)
   fclose ( fp );
   printf ( "Words count: %d\n", (*t).n_total_palavras );
 
-  (*t).palavras = (char**) malloc(sizeof(char*) * n_max_caracteres); /* -- INSERT code for memory allocation --*/;
+  (*t).palavras = (char**) malloc(sizeof(char*) * (*t).n_total_palavras); /* -- INSERT code for memory allocation --*/;
   if ( (*t).palavras == NULL ) {
     fprintf ( stderr, "ERROR: not enough memory available!\n" );
     exit ( 2 );
   }
-  (*t).ocorrencias = (char*) malloc(sizeof(char) * MAX_STR); /* -- INSERT code for memory allocation --*/;
+  (*t).ocorrencias = (char*) malloc(sizeof(char) * (*t).n_total_palavras); /* -- INSERT code for memory allocation --*/;
   if ( (*t).ocorrencias == NULL ) {
     fprintf ( stderr, "ERROR: not enough memory available!\n" );
     exit ( 4 );
   }
   for ( i = 0; i < (*t).n_total_palavras; i++ )   {
-    (*t).palavras[i] = (char*) malloc(sizeof(char) * (*t).ocorrencias[i]);  /* -- INSERT code for memory allocation --*/;
+    (*t).palavras[i] = (char*) malloc(sizeof(char) * n_max_caracteres +1);  /* -- INSERT code for memory allocation --*/;
     if ( (*t).palavras[i] == NULL ) {
       fprintf ( stderr, "ERROR: not enough memory available!\n" );
       exit ( 3 );
