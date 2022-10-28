bf563813e30688dc6ed0ba3d6d777c3d394668ec
Alex-Ares <ares.santos@tecnico.ulisboa.pt>
2022-10-28 18:51:57 +0100

    updated head
    head updates

--- Projeto/2º entrega/garfo.h
+++ Projeto/2º entrega/garfo.h
@@ -16,7 +16,21 @@ typedef struct node {
     int wt;
 } node;
 
+typedef struct head{
+
+    Item **heads;
+    int wt;
+    int nv
+
+} head;
+
 garfo* garfointit( int nv);
 void putingarfo(garfo* g,Item v[],int max_wt,int test(Item,Item));
 void facagarfo(garfo* g);
 int test(Item w1,Item w2);
+Item headinit(int nv);
+void puthead(head* a,head *b);
+void fixheadup(head*a);
+void fixheadown(head*a);
+Item* gethead(head*a, int max_wt);
+
